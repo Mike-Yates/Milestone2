5b5b566db7b038a5dca304e8e08dd67a6bfe1d25
Mike-Yates <70710481+Mike-Yates@users.noreply.github.com>
2022-03-17 16:39:24 -0400

    added a few insert commands

--- milestone2.sql
+++ milestone2.sql
@@ -1,5 +1,49 @@
 -- Michael Yates (mjy5xy) and Justin Logan (jhl3mn)
+-- Post(post_id, Datetime, text, votes, post_report, post_type) 
 
+CREATE TABLE IF NOT EXISTS Post  ( -- [Post Table]
+    post_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    post_time Datetime NOT NULL,
+    post_text VARCHAR(99) NOT NULL,
+    post_report INT NOT NULL,
+    post_type VARCHAR(10) NOT NULL, -- text, poll, video, or picture
+    PRIMARY KEY (post_id)
+);
+INSERT INTO Post VALUES (NULL, CURRENT_TIMESTAMP - 5, 'my name is mike, and this is my first post.', 0, 'text');
+INSERT INTO Post VALUES (NULL, CURRENT_TIMESTAMP - 10, 'my name is justin, check out this picture!!', 0, 'picture');
+INSERT INTO Post VALUES (NULL, CURRENT_TIMESTAMP - 20, 'UVA Media is the Next Big Thing!! yes or no?', 0, 'poll');
+INSERT INTO Post VALUES (NULL, CURRENT_TIMESTAMP - 30, 'what do you think of this video', 0, 'video');
+
+
+-- Comment(Post_id, comment_text) 
+CREATE TABLE IF NOT EXISTS Comment ( -- [Comment Table]
+    post_id INT UNSIGNED NOT NULL,
+    post_text VARCHAR(99) NOT NULL,
+    PRIMARY KEY (post_id)
+);
+
+-- Text_Post(Post_id, color)
+CREATE TABLE IF NOT EXISTS Text_Post ( -- [Text_Post Table]
+    post_id INT UNSIGNED NOT NULL,
+    color VARCHAR(30) NOT NULL,
+    PRIMARY KEY (post_id)
+);
+
+-- Images(Post_id, image)
+CREATE TABLE IF NOT EXISTS Images ( -- [Images Table]
+    post_id INT UNSIGNED NOT NULL,
+    content varchar(1024),  -- store a reference to where the image is saved. 
+    PRIMARY KEY (post_id)
+);
+
+-- Videos(Post_id, video)
+CREATE TABLE IF NOT EXISTS Videos ( -- [Images Table]
+    post_id INT NOT NULL,
+    content varchar(1024),  -- store a reference to where the video is saved. 
+    PRIMARY KEY (post_id)
+);
+
+<<<<<<< Updated upstream
 -- Post(post_id, Datetime, text, votes, post_report, post_type) 
 CREATE TABLE IF NOT EXISTS Post  ( -- [Post Table]
     post_id INT NOT NULL,
@@ -30,14 +74,52 @@ CREATE TABLE IF NOT EXISTS Images ( -- [Images Table]
     color VARCHAR(30) NOT NULL,
     content varchar(1024),  -- store a reference to where the image is saved. 
     PRIMARY KEY (post_id)
-);
-
--- Videos(Post_id, video)
-CREATE TABLE IF NOT EXISTS Images ( -- [Images Table]
+=======
+--Poll_Post(Post_id, num_options)
+CREATE TABLE IF NOT EXISTS Poll_Post  ( -- [Poll_Post Table]
     post_id INT NOT NULL,
-    content varchar(1024),  -- store a reference to where the video is saved. 
+    num_options INT NOT NULL,
     PRIMARY KEY (post_id)
 );
+--poll_options(Post_id, option_id, option_name, vote_count)
+CREATE TABLE IF NOT EXISTS Poll_Options  ( -- [Poll_Options Table]
+    post_id INT NOT NULL,
+    option_id INT NOT NULL,
+    option_name VARCHAR(30) NOT NULL,
+    vote_count INT NOT NULL,
+    PRIMARY KEY (post_id, option_id)
+);
+--ActiveUser(ActivePhoneNumber, points)
+CREATE TABLE IF NOT EXISTS Active_User  ( -- [Active_User Table]
+    phone_number BIGINT NOT NULL,
+    points INT NOT NULL,
+    PRIMARY KEY (phone_number)
+);
+--BannedUser(BannedPhoneNumber, Reason)
+CREATE TABLE IF NOT EXISTS Banned_User  ( -- [Banned_User Table]
+    phone_number BIGINT NOT NULL,
+    reason VARCHAR(99) NOT NULL,
+    PRIMARY KEY (phone_number)
+);
+--SuspendedUser(BannedPhoneNumber, Reason) 
+CREATE TABLE IF NOT EXISTS Suspended_User  ( -- [Suspended_User Table]
+    phone_number BIGINT NOT NULL,
+    reason VARCHAR(99) NOT NULL,
+    sus_time INT NOT NULL, --in seconds
+    PRIMARY KEY (phone_number)
+);
+--AdminUser(AdminPhoneNumber, username) 
+CREATE TABLE IF NOT EXISTS Admin_User  ( -- [Admin_User Table]
+    phone_number BIGINT NOT NULL,
+    username VARCHAR(30) NOT NULL,
+    PRIMARY KEY (phone_number)
+>>>>>>> Stashed changes
+);
 
-
+-- PostCreator(post_id, phone_num)
+CREATE TABLE IF NOT EXISTS Post_Creator  ( -- [Post_Creator Table]
+    phone_number BIGINT NOT NULL,
+    int post_id NOT NULL, 
+    PRIMARY KEY (post_id)
+);
 
