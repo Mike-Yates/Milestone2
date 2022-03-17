2bd4f6e0e921899fa96c4dcd856b007517c4ebf6
justinlogan03 <jhl3mn@virginia.edu>
2022-03-17 18:18:13 -0400

    First round of SQL
    Fill in data to use, options 6-11

--- milestone2.sql
+++ milestone2.sql
@@ -69,6 +69,12 @@ CREATE TABLE IF NOT EXISTS Poll_Post  ( -- [Poll_Post Table]
     num_options INT NOT NULL,
     PRIMARY KEY (post_id)
 );
+
+INSERT INTO Poll_Post VALUES (8, 2);
+INSERT INTO Poll_Post VALUES (9, 4);
+INSERT INTO Poll_Post VALUES (10, 4);
+INSERT INTO Poll_Post VALUES (11, 3);
+
 --poll_options(Post_id, option_id, option_name, vote_count)
 CREATE TABLE IF NOT EXISTS Poll_Options  ( -- [Poll_Options Table]
     post_id INT UNSIGNED NOT NULL,
@@ -77,18 +83,48 @@ CREATE TABLE IF NOT EXISTS Poll_Options  ( -- [Poll_Options Table]
     vote_count INT NOT NULL,
     PRIMARY KEY (post_id, option_id)
 );
+
+--'UVA Media is the Next Big Thing!! yes or no?
+INSERT INTO Poll_Options VALUES (8, 0, 'yes', 0);
+INSERT INTO Poll_Options VALUES (8, 1, 'no', 0);
+--'Best soccer player?'
+INSERT INTO Poll_Options VALUES (9, 0, 'Messi', 0);
+INSERT INTO Poll_Options VALUES (9, 1, 'Ronaldo', 0);
+INSERT INTO Poll_Options VALUES (9, 2, 'Maguire', 0);
+INSERT INTO Poll_Options VALUES (9, 3, 'Mbappe', 0);
+--'Best bastetball player in the East?
+INSERT INTO Poll_Options VALUES (10, 0, 'Tatum', 0);
+INSERT INTO Poll_Options VALUES (10, 1, 'Embiid', 0);
+INSERT INTO Poll_Options VALUES (10, 2, 'Antetokounmpo', 0);
+INSERT INTO Poll_Options VALUES (10, 3, 'Durant', 0);
+--favorite food?
+INSERT INTO Poll_Options VALUES (11, 0, 'Tacos', 0);
+INSERT INTO Poll_Options VALUES (11, 1, 'Pizza', 0);
+INSERT INTO Poll_Options VALUES (11, 2, 'Ice Cream', 0);
+
 --ActiveUser(ActivePhoneNumber, points)
 CREATE TABLE IF NOT EXISTS Active_User  ( -- [Active_User Table]
     phone_number BIGINT NOT NULL,
     points INT NOT NULL,
     PRIMARY KEY (phone_number)
 );
+
+INSERT INTO Active_User VALUES (5715550101, 10);
+INSERT INTO Active_User VALUES (7035550202, 12);
+INSERT INTO Active_User VALUES (2035550303, 3);
+INSERT INTO Active_User VALUES (5715550404, 22);
+
+
 --BannedUser(BannedPhoneNumber, Reason)
 CREATE TABLE IF NOT EXISTS Banned_User  ( -- [Banned_User Table]
     phone_number BIGINT NOT NULL,
     reason VARCHAR(99) NOT NULL,
     PRIMARY KEY (phone_number)
 );
+
+INSERT INTO Banned_User VALUES (2035550505, "profanity");
+INSERT INTO Banned_User VALUES (7035550606, "not cool enough");
+
 --SuspendedUser(BannedPhoneNumber, Reason) 
 CREATE TABLE IF NOT EXISTS Suspended_User  ( -- [Suspended_User Table]
     phone_number BIGINT NOT NULL,
@@ -96,12 +132,19 @@ CREATE TABLE IF NOT EXISTS Suspended_User  ( -- [Suspended_User Table]
     sus_time INT NOT NULL, --in seconds
     PRIMARY KEY (phone_number)
 );
+
+INSERT INTO Suspended_User VALUES (4345550707, "inapropriate", 604800);
+INSERT INTO Suspended_User VALUES (5715550808, "illegal", 1209600);
+
 --AdminUser(AdminPhoneNumber, username) 
 CREATE TABLE IF NOT EXISTS Admin_User  ( -- [Admin_User Table]
     phone_number BIGINT NOT NULL,
     username VARCHAR(30) NOT NULL,
     PRIMARY KEY (phone_number)
 );
+
+INSERT INTO Admin_User VALUES (5715550909, "Justin_Is_Cool");
+INSERT INTO Admin_User VALUES (7035551010, "MikeCR7");
 
 -- PostCreator(post_id, phone_num)
 CREATE TABLE IF NOT EXISTS Post_Creator  ( -- [Post_Creator Table]
@@ -110,3 +153,19 @@ CREATE TABLE IF NOT EXISTS Post_Creator  ( -- [Post_Creator Table]
     PRIMARY KEY (post_id)
 );
 
+INSERT INTO Post_Creator VALUES (5715550101, 0);
+INSERT INTO Post_Creator VALUES (7035550202, 1);
+INSERT INTO Post_Creator VALUES (2035550303, 2);
+INSERT INTO Post_Creator VALUES (5715550404, 3);
+INSERT INTO Post_Creator VALUES (5715550101, 4);
+INSERT INTO Post_Creator VALUES (7035550202, 5);
+INSERT INTO Post_Creator VALUES (2035550303, 6);
+INSERT INTO Post_Creator VALUES (5715550404, 7);
+INSERT INTO Post_Creator VALUES (5715550101, 8);
+INSERT INTO Post_Creator VALUES (7035550202, 9);
+INSERT INTO Post_Creator VALUES (2035550303, 10);
+INSERT INTO Post_Creator VALUES (5715550404, 11);
+INSERT INTO Post_Creator VALUES (2035550303, 12);
+INSERT INTO Post_Creator VALUES (5715550404, 13);
+INSERT INTO Post_Creator VALUES (2035550303, 14);
+INSERT INTO Post_Creator VALUES (5715550404, 15);
